824eae2e1c1ad250b0b50443e193ac16d3e04f6e
牛玉虎 <niuyuhu1@huawei.com>
2025-12-31 10:25:12 +0800

    define AFIR dialect

--- include/Dialect/AFIR/AFIRDialect.h
+++ include/Dialect/AFIR/AFIRDialect.h
@@ -9,6 +9,18 @@
 
 #include "mlir/IR/Dialect.h"
 
+// Include dialect definition
 #include "Dialect/AFIR/AFIRDialect.h.inc"
+
+// Include enums (must be before attributes)
+#include "Dialect/AFIR/AFIREnums.h.inc"
+
+// Include attribute definitions
+#define GET_ATTRDEF_CLASSES
+#include "Dialect/AFIR/AFIRAttrs.h.inc"
+
+// Include type definitions
+#define GET_TYPEDEF_CLASSES
+#include "Dialect/AFIR/AFIRTypes.h.inc"
 
 #endif  // MLIR_DIALECT_AFIR_AFIRDIALECT_H
