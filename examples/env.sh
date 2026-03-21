97fcf775570cc49012e1a18acab8ed0c1438d5cf
牛玉虎 <niuyuhu1@huawei.com>
2026-03-21 10:51:53 +0800

    update env

--- examples/env.sh
+++ examples/env.sh
@@ -1,1 +1,7 @@
 export PATH=$PATH:/home/niu/code/Ascend-MLIR/build/bin
+
+# CANN toolkit（通过 Mac 挂载路径访问）
+CANN_BASE="/mnt/mac/Volumes/GM9/cann/Ascend/20251209_newest/ascend-toolkit/latest"
+if [ -d "$CANN_BASE" ]; then
+  export ASCEND_HOME_PATH="$CANN_BASE"
+fi
