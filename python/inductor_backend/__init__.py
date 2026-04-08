2a0732fe2440d695615630de680bd36f4e8220d8
gaoshuer <gaoshuer@huawei.com>
2026-04-08 14:57:23 +0800

    Revert "feat(inductor): add base structure for inductor-mlir backend"
    This reverts commit 721fde1cab5e2915caa811f15e3e9d918d374359.

--- python/inductor_backend/__init__.py
+++
@@ -1,4 +0,0 @@
-from .scheduling import MLIRScheduling, setup_inductor_backend
-from .post_fusion_pass import create_post_fusion_pass
-
-__all__ = ['MLIRScheduling', 'setup_inductor_backend', 'create_post_fusion_pass']
