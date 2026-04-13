7a9d318f9b2556d77cc7418f5bb2677a45574462
牛玉虎 <niuyuhu1@huawei.com>
2026-04-13 21:22:35 +0800

    fix: restore autotuner memory buffer include

--- tools/autotuner/autotuner_main.cpp
+++ tools/autotuner/autotuner_main.cpp
@@ -12,6 +12,7 @@
 #include "llvm/Support/CommandLine.h"
 #include "llvm/Support/FileSystem.h"
 #include "llvm/Support/JSON.h"
+#include "llvm/Support/MemoryBuffer.h"
 #include "llvm/Support/Path.h"
 #include "llvm/Support/raw_ostream.h"
 #include <algorithm>
