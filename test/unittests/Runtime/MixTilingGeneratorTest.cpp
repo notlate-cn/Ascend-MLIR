46d3fea686cac7d2c38e3c9d9f629a8601d72e4e
牛玉虎 <niuyuhu1@huawei.com>
2026-04-16 14:48:31 +0800

    runtime: drop fragile matmul mix test hook

--- test/unittests/Runtime/MixTilingGeneratorTest.cpp
+++ test/unittests/Runtime/MixTilingGeneratorTest.cpp
@@ -9,13 +9,6 @@
 
 using namespace mlir::runtime;
 
-namespace mlir::runtime {
-#if defined(MLIR_RUNTIME_ENABLE_MATMUL_MIX_ADAPTER_TEST_HOOKS)
-llvm::Expected<MatmulTilingRequest>
-buildMatmulApiTilingRequestForTest(const MixTilingRequest &request);
-#endif
-} // namespace mlir::runtime
-
 namespace {
 
 MixTilingRequest makeRequestWithUnsupportedBiasDType() {
@@ -47,30 +40,4 @@ TEST(MixTilingGeneratorTest, RejectsUnsupportedBiasDTypeThroughAdapter) {
   ASSERT_FALSE(messages.empty());
   EXPECT_NE(messages[0].find("unsupported matmul api tiling request"),
             std::string::npos);
-}
-
-TEST(MixTilingGeneratorTest, PreservesSupportedExplicitBiasDTypeThroughAdapter) {
-  MixTilingRequest request;
-  request.kernelName = "matmul_bias_relu";
-  request.socVersion = "Ascend910B1";
-  request.inputs = {
-      {DType::F16, {16, 64}},
-      {DType::BF16, {64, 32}},
-      {DType::BF16, {32}},
-  };
-  request.outputs = {
-      {DType::F32, {16, 32}},
-  };
-
-#if defined(MLIR_RUNTIME_ENABLE_MATMUL_MIX_ADAPTER_TEST_HOOKS)
-  auto matmulRequestOr = buildMatmulApiTilingRequestForTest(request);
-  ASSERT_TRUE(static_cast<bool>(matmulRequestOr));
-  EXPECT_TRUE(matmulRequestOr->problem.biasDType.has_value());
-  EXPECT_EQ(*matmulRequestOr->problem.biasDType, DType::BF16);
-#endif
-
-  auto result = generateMixTilingInProcess(request);
-  ASSERT_TRUE(static_cast<bool>(result));
-  EXPECT_GT(result->blockDim, 0u);
-  EXPECT_FALSE(result->tilingData.empty());
 }
