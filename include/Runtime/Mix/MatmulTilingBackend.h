d5df026e9f265df8589569a80574b063c7475ce5
牛玉虎 <niuyuhu1@huawei.com>
2026-04-16 13:01:13 +0800

    runtime: preserve matmul tiling backend errors

--- include/Runtime/Mix/MatmulTilingBackend.h
+++ include/Runtime/Mix/MatmulTilingBackend.h
@@ -12,6 +12,10 @@ public:
   virtual ~MatmulTilingBackend() = default;
 
   virtual llvm::StringRef name() const = 0;
+  // `supports()` should be a cheap capability check for the same request
+  // passed to `generate()`. If it returns true, `generate()` is expected to
+  // either produce a result for that request or return a detailed `llvm::Error`
+  // explaining why the backend could not finish.
   virtual bool supports(const MatmulTilingRequest &request) const = 0;
   virtual llvm::Expected<MatmulTilingResult>
   generate(const MatmulTilingRequest &request) const = 0;
