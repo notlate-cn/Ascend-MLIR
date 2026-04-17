a80b31b60365869ab97b0d04b1c5f296d890ca5d
牛玉虎 <niuyuhu1@huawei.com>
2026-04-17 04:10:47 -0700

    runtime: restore scheduler contract

--- include/Runtime/Execution/ResourceScheduler.h
+++ include/Runtime/Execution/ResourceScheduler.h
@@ -14,10 +14,10 @@ struct TaskResourceRequirement {
   ExecutionBackendKind backendKind = ExecutionBackendKind::Simulation;
   size_t workspaceBytes = 0;
   bool requiresSerializedLaunch = false;
+  bool exclusiveDeviceAccess = false;
 };
 
 struct ResourceReservation {
-  uint64_t reservationId = 0;
   std::string sessionId;
   std::string taskId;
   ExecutionBackendKind backendKind = ExecutionBackendKind::Simulation;
@@ -39,8 +39,27 @@ public:
   void release(const ResourceReservation &reservation);
 
 private:
+  struct ReservationKey {
+    std::string sessionId;
+    std::string taskId;
+    ExecutionBackendKind backendKind = ExecutionBackendKind::Simulation;
+    size_t workspaceBytes = 0;
+    bool holdsSerializedLaunchLane = false;
+    bool holdsDeviceSlot = false;
+  };
+
+  struct ReservationKeyHash {
+    size_t operator()(const ReservationKey &key) const;
+  };
+
+  struct ReservationKeyEq {
+    bool operator()(const ReservationKey &lhs,
+                    const ReservationKey &rhs) const;
+  };
+
   struct ActiveReservation {
     size_t workspaceBytes = 0;
+    size_t deviceSlots = 0;
     bool holdsSerializedLaunchLane = false;
     bool holdsDeviceSlot = false;
   };
@@ -51,8 +70,9 @@ private:
   size_t reservedSimDispatchLanes_ = 0;
   size_t reservedDeviceSlots_ = 0;
   size_t reservedWorkspaceBytes_ = 0;
-  uint64_t nextReservationId_ = 1;
-  std::unordered_map<uint64_t, ActiveReservation> activeReservations_;
+  std::unordered_multimap<ReservationKey, ActiveReservation, ReservationKeyHash,
+                          ReservationKeyEq>
+      activeReservations_;
 };
 
 } // namespace mlir::runtime
